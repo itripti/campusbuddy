5b85b9d1f77ae8768c383d46859a3be27fb8ab97
Tripti <your-email@example.com>
2026-06-18 01:48:36 +0530

    added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,13 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://campusbuddy-three.vercel.app',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Base Route
